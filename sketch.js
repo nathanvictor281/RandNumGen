517f77faed333c7f49962176a93e2fe78d746c1f
Nv <130412927+nathanvictor281@users.noreply.github.com>
2023-05-27 20:14:04 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,18 +4,46 @@ function setup() {
 
   const generateButton = select('#generateButton');
   generateButton.mousePressed(generateRandomNumber);
+
+  const diceButton = select('#numClicks');
+  diceButton.mousePressed(rollDice);
+}
+
+let numClicks = 1;
+let minNumber = 1;
+let maxNumber = 18;
+
+// Roll the dice to determine the number of clicks
+function rollDice() {
+  numClicks = Math.floor(random(1, 7));
+  const diceButton = select('#numClicks');
+  diceButton.html(`&#9856; ${numClicks}`);
 }
 
 // Generate a random number
 function generateRandomNumber() {
-  const minNumber = parseInt(select('#minNumber').value());
-  const maxNumber = parseInt(select('#maxNumber').value());
-
-  const randomNumber = Math.floor(random(minNumber, maxNumber + 1));
-  
   const numberDisplay = select('#numberDisplay');
-  numberDisplay.html(randomNumber);
+
+  if (minNumber <= maxNumber) {
+    const randomNumber = Math.floor(random(minNumber, maxNumber + 1));
+    numberDisplay.html(randomNumber);
+  } else {
+    numberDisplay.html('Invalid Range');
+  }
 }
+
+// Update the minimum and maximum values
+function updateRange() {
+  minNumber = parseInt(select('#minNumber').value());
+  maxNumber = parseInt(select('#maxNumber').value());
+}
+
+// Event listener for range inputs
+const minNumberInput = select('#minNumber');
+minNumberInput.input(updateRange);
+
+const maxNumberInput = select('#maxNumber');
+maxNumberInput.input(updateRange);
 
 
 
